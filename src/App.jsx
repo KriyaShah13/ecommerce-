1e07fcee59258629c5f3e0695134bd13a6dbeb28
Kriya Shah <shahkriya0508@gmail.com>
2024-03-09 20:31:52 +0530

    initialised project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navigation from "./Component/Navigation";
+import Hero from "./Component/Hero";
+const App = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Hero></Hero>
+            
+        </div>
+    );
+};
+export default App;
---
+++ src/Component/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+    return (
+        <nav>
+                <div className="Logo">
+                    <img src="/images/brand_logo.png" alt="" />
+                </div>
+               <ul>
+                <li href = "#"> Menu</li>
+                <li href = "#"> Contact</li>
+                <li href = "#"> About</li>
+                <li href = "#"> Location </li>
+               </ul>
+               <button>Login</button>
+            </nav>
+    )
+}
+export default Navigation;
